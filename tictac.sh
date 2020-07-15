898d4a953ae74618e020e6f970b39071f0ec3bcd
mrunal93 <mrn26893@gmail.com>
2020-07-15 05:34:31 +0530

    [Mrunal]Add: generate a toss and checked the turn

--- tictac.sh
+++ tictac.sh
@@ -5,7 +5,7 @@ echo -e "Welcome to Tic-Tac-Toe Game \n----------------------------- \nAs a Tic 
 ROW_SIZE=3
 BOARD_SIZE=$((ROW_SIZE*ROW_SIZE))
 Position=0
-declare -a ticBoard
+declare -A ticBoard
 
 resetBoard() {
 	for (( position=1; position<=$BOARD_SIZE; position++ ))
@@ -14,3 +14,17 @@ resetBoard() {
 	done
 }
 
+tossFirstPlayer(){
+	randomCheck=$((RANDOM%2))
+	if [ $randomCheck -eq $user_symbol ]
+	then
+		firstPlayer=human
+		echo "HUMAN has won the Toss"
+	else
+		firstPlayer=Computer
+		echo "COMPUTER has won the Toss"
+	fi
+
+}
+
+tossFirstPlayer
